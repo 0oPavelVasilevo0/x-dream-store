eb37b05f8f01609329a5bdf47dc707ef9f5f6d3c
Pavel Vasilev <karterpahan@gmail.com>
2024-06-04 17:16:22 +0200

    changing logic for email confirmation

--- app/(pages)/create_account/page.tsx
+++ app/(pages)/create_account/page.tsx
@@ -1,38 +1,114 @@
-"use client"
-import axios from "axios";
-import { useState } from "react";
+'use client'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useRouter } from "next/navigation";
 import { Box, Button, Divider, Paper, TextField, Typography } from "@mui/material";
-import Link, { NextLinkComposed } from "@/app/components/link/Link";
+import Link from "@/app/components/link/Link";
 
 export default function CreateAccount() {
+    const [step, setStep] = useState(1); // Step 1: enter email, Step 2: enter verification code, Step 3: create account
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState("");
+    const [verificationCode, setVerificationCode] = useState('');
+    const [isSending, setIsSending] = useState(false);
+    const [successMessage, setSuccessMessage] = useState('');
+    const [error, setError] = useState('');
+    const [countdown, setCountdown] = useState(0); // 15 minutes in seconds
     const router = useRouter();
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const handleEmailSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setIsSending(true);
+        setSuccessMessage('');
+        setError('');
         try {
-            const response = await axios.post('/api/users', {
+            const response = await axios.post('/api/users-create', { email });
+            console.log(response.data);
+            if (response.status === 200) {
+                // Обработка успешного ответа
+                // Display the message
+                setSuccessMessage(response.data.message);
+                const expirationTime = new Date(response.data.expiresAt).getTime();
+                const currentTime = new Date().getTime();
+                const remainingTime = Math.floor((expirationTime - currentTime) / 1000);
+                setCountdown(remainingTime);
+                setTimeout(() => {
+                    setStep(2);
+                    setSuccessMessage('');
+                }, 5000); // Show message for 5 seconds before switching steps
+            } else {
+                // Обработка ошибки
+                setError(response.data.message);
+            }
+        } catch (error: any) {
+            setError(error.response?.data?.message || 'An error occurred');
+        }
+        finally {
+            setIsSending(false);
+        }
+    };
+
+    const handleVerificationSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setSuccessMessage('');
+        setIsSending(true);
+        setError('');
+        try {
+            const response = await axios.post('/api/users-verif', {
                 name,
                 email,
                 password,
+                verificationCode,
             });
-            router.push('/login');
-        } catch (error: any) {
-            if (error.response.status === 400) {
-                // Handle 400 Bad Request (email already exists)
-                setError("Email already exist");
+            console.log(response.data);
+            if (response.status === 201) {
+                // Обработка успешного ответа
+                setSuccessMessage(response.data.message);
+                setTimeout(() => {
+                    router.push('/login');
+                }, 5000);
             } else {
-                // Handle other errors
-                console.log("Error creating user with account: ", error.response);
-                setError("An error occurred");
+                // Обработка ошибки
+                setError(response.data.message);
             }
+        } catch (error: any) {
+            setError(error.response?.data?.message);
         }
-    }
+        finally {
+            setIsSending(false);
+        }
+    };
 
+    useEffect(() => {
+        let timer: number;
+        if (countdown > 0) {
+            timer = window.setInterval(() => {
+                setCountdown((prevCountdown) => {
+                    if (prevCountdown <= 1) {
+                        clearInterval(timer);
+                        setStep(1);
+                        setError('Verification code expired. Please request a new code.');
+                        return 0;
+                    }
+                    return prevCountdown - 1;
+                });
+            }, 1000);
+        }
+        return () => clearInterval(timer);
+    }, [countdown]);
+
+    const TimerCode = (
+        <>
+            {step === 2 && countdown > 0 && (
+                <>
+                    <Typography sx={{ mt: 3 }} >
+                        code expires in<span style={{ color: 'red', fontSize: 18, fontWeight: 'bold' }}> {Math.floor(countdown / 60)}:{('0' + (countdown % 60)).slice(-2)}</span>
+                    </Typography>
+                </>
+            )}
+        </>
+    )
 
     return (
         <Box sx={{
@@ -51,90 +127,130 @@ export default function CreateAccount() {
                 p: 1,
 
             }}>
-                <Box sx={{ bgcolor: error ? 'red' : null, borderRadius: error ? 0.5 : null }}>
+                <Box sx={{ bgcolor: error ? 'red' : null, borderRadius: error ? 1 : null, padding: error ? '6px 16px' : null }}>
                     {(error === "") ? (
-                        <Typography variant="h6" fontSize={22} sx={{ color: 'cyan' }} >
-                            Create Aсcount
-                        </Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', }}>
+                            <Typography variant="button" fontSize={20} sx={{ color: 'cyan' }} >
+                                create
+                            </Typography>
+                            <Typography variant="button" fontSize={12} >
+                                account
+                            </Typography>
+                        </Box>
                     ) : (
-                        <Typography variant="h6" fontSize={22} sx={{ color: 'white' }}>
+                        <Typography variant="button" fontSize={14} sx={{ color: 'white' }}>
                             {error}
                         </Typography>)}
                 </Box>
-                <Divider />
-                <Typography fontSize={14} >
-                    Do you have an account?{" "}
+                <Divider sx={{ display: error ? 'none' : null }} />
+                <Typography variant="button" fontSize={12} >
+                    do You have an account?{" "}
                     <Link
                         href="/login"
                         underline="none"
                         activeClassName="false"
                         sx={{ color: 'cyan', fontSize: 16 }}
                     >
-                        Log in
+                        LOG IN
                     </Link>
                 </Typography>
-                <form onSubmit={handleSubmit}>
-                    <TextField
-                        fullWidth
-                        size="small"
-                        color="warning"
-                        id="name"
-                        label="Name"
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                        autoComplete="off"
-                        sx={{ mt: 4 }}
-                    />
-                    <TextField
-                        fullWidth
-                        size="small"
-                        color="warning"
-                        id="email"
-                        label="Email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        autoComplete="off"
-                        sx={{ mt: 2 }}
-                    />
-                    <TextField
-                        fullWidth
-                        size="small"
-                        color="warning"
-                        id="password"
-                        label="Password"
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                        autoComplete="off"
-                        sx={{ mt: 2 }}
-                    />
-                    <Button
-                        fullWidth
-                        type="submit"
-                        variant="contained"
-                        color="warning"
-                        sx={{ mt: 3 }}
-                    >
-                        Create User
-                    </Button>
-                </form>
-                {/* <Divider sx={{ my: 2 }}>
-                    or
-                </Divider>
-                <Button
-                    fullWidth
-                    component={NextLinkComposed}
-                    to="/login"
-                    variant="outlined"
-                    color="inherit"
-                >
-                    Log in to your account
-                </Button> */}
+                {successMessage === '' ? (
+                    <>
+                        {step === 1 && (
+                            <form onSubmit={handleEmailSubmit}>
+                                <TextField
+                                    fullWidth
+                                    size="small"
+                                    color="warning"
+                                    type="email"
+                                    id="email"
+                                    label="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                    autoComplete="off"
+                                    sx={{ mt: 4 }}
+                                />
+                                <Button
+                                    fullWidth
+                                    type="submit"
+                                    variant="contained"
+                                    color="warning"
+                                    disabled={isSending}
+                                    sx={{ mt: 3 }}
+                                >
+                                    {isSending ? 'Sending Code...' : 'Send Verification Code'}
+                                </Button>
+                            </form>
+                        )}
+                        {step === 2 && (
+                            <>
+                                {TimerCode}
+                                <form onSubmit={handleVerificationSubmit}>
+                                    <TextField
+                                        fullWidth
+                                        size="small"
+                                        color="warning"
+                                        type="text"
+                                        id="verificationCode"
+                                        label="Verification Code"
+                                        value={verificationCode}
+                                        onChange={(e) => setVerificationCode(e.target.value)}
+                                        required
+                                        autoComplete="off"
+                                        sx={{ mt: 1 }}
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        size="small"
+                                        color="warning"
+                                        id="name"
+                                        label="Name"
+                                        type="text"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        required
+                                        autoComplete="off"
+                                        sx={{ mt: 2 }}
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        size="small"
+                                        color="warning"
+                                        id="password"
+                                        label="Password"
+                                        type="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                        autoComplete="off"
+                                        sx={{ mt: 2 }}
+                                    />
+                                    <Button
+                                        fullWidth
+                                        type="submit"
+                                        variant="contained"
+                                        color="warning"
+                                        sx={{ mt: 3 }}
+                                        disabled={isSending}
+                                    >
+                                        {isSending ? 'Creating a User...' : 'Create User'}
+                                    </Button>
+                                </form>
+                            </>
+                        )}
+                    </>
+                ) : (
+                    <Box sx={{ borderRadius: 1, border: 1, borderColor: 'yellow', mt: 4, padding: '6px 16px' }}>
+                        <Typography
+                            variant="button"
+                            fontSize={14}
+                            sx={{ color: 'yellow' }}
+                        >
+                            {successMessage}
+                        </Typography>
+                    </Box>
+                )}
             </Box>
         </Box>
     )
