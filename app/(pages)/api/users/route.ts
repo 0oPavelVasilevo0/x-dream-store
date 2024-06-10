3ac237ce526c1f5c0c7f52d7c4ed5ed7cf90d5b7
Pavel Vasilev <karterpahan@gmail.com>
2024-06-10 10:24:41 +0200

    fix api users

--- app/(pages)/api/users/route.ts
+++ app/(pages)/api/users/route.ts
@@ -1,12 +1,11 @@
 import { createUserWithAccount, getUserByEmail } from '@/app/utils/user';  // Import user-related utilities
 import bcrypt from 'bcryptjs'; // Import bcryptjs instead of bcrypt
-import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: NextRequest, res: NextResponse) => {                  //res?
+// export const POST = async (req: NextRequest, res: NextResponse) => {                     //res?
+export const POST = async (req: NextRequest) => {
     try {
         const { name, email, password } = await req.json();
-        // const { name, email, password } = req.body; 
 
         // Check if the email already exists in the database
         const existingUser = await getUserByEmail(email);
@@ -16,8 +15,16 @@ export const POST = async (req: NextRequest, res: NextResponse) => {            
             }, { status: 400 });
         }
 
+        // Check if the password has at least 10 characters
+        if (password.length < 10) {
+            return NextResponse.json({
+                message: "Password must be at least 10 characters long.",
+            }, { status: 400 });
+        }
+
         // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10); // Salt round of 10
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt); // Salt round of 10
 
         // Create the new user if the email doesn't exist
         const newUser = await createUserWithAccount({
